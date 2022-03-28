fdff8d8dc005d4f0cd7873ebcdff1908c356ed6a
ishfaq <you@example.com>
2022-03-28 13:58:46 +0500

    perfect crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import {Link,useHistory ,BrowserRouter as Router,Switch,Route}  from 'react-router-dom'
+import {Home}  from './components/Home'
+export const App = () => {
+ // const {histrory}=useHistory()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+      <ul>
+        <li>
+       
+          <Link to='/Home?limit=10&&offset=0'>Home</Link>
+        </li>
+      </ul>
+    
+            
+            <Switch>
+    <Route path="/Home" component={Home} exact  />
+  
+        </Switch>
+          
+        </Router>
+      
+        
     </div>
-  );
+  )
 }
-
-export default App;
